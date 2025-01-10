a494f863a1ae23a15b4d521a9be04d477e1b7d60
lusc8520 <lukas-schuenemann@web.de>
2025-01-10 22:50:00 +0100

    Correct url

--- react_client/src/hooks/useBaseUrl.ts
+++ react_client/src/hooks/useBaseUrl.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const possibleUrls = ["3.66.158.107:3000", "http://localhost:3000"];
+const possibleUrls = ["http://3.66.158.107:3000", "http://localhost:3000"];
 
 export function useBaseUrl() {
   const [baseUrl, setBaseUrl] = useState<string | undefined>(undefined);
