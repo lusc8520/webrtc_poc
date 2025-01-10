6788c36cee0e3b91b1427d256d71a4f0c725c230
lusc8520 <lukas-schuenemann@web.de>
2025-01-11 00:25:38 +0100

    Use public url

--- react_client/src/hooks/useBaseUrl.ts
+++ react_client/src/hooks/useBaseUrl.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const possibleUrls = ["http://3.66.158.107", "http://localhost:3000"];
+const possibleUrls = ["https://shuneman.net", "http://localhost:3000"];
 
 export function useBaseUrl() {
   const [baseUrl, setBaseUrl] = useState<string | undefined>(undefined);
